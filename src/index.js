858b7173ab1feb415db3c676122cdb75ebd4df14
ilia-mh <iliya.numb@gmail.com>
2022-09-09 22:21:15 +0200

    implemented smooth scroll and started developing cursor class

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
 import './assets/main.scss'
+import initSmoothScrolling from './utils/initializeSmoothScroll'
+import gsap from 'gsap'
+import ScrollTrigger from "gsap/ScrollTrigger";
+import { setProxyOnGsapScrollTrigger } from './utils/setScrollProxy';
+
+gsap.registerPlugin(ScrollTrigger);
+
+document.addEventListener("DOMContentLoaded", () => {
+
+  const scrollBar = initSmoothScrolling()
+
+  setProxyOnGsapScrollTrigger(scrollBar)
+})
